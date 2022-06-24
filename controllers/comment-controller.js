9cd2e4fefda58fb079343440aef67073e6a549b4
kgil60 <gilvaryke@gmail.com>
2022-06-23 23:49:22 -0400

    add comments to pizzas

---
+++ controllers/comment-controller.js
@@ -0,0 +1,51 @@
+const { resourceLimits } = require('worker_threads');
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+    // add comment to pizza
+    addComment({ params, body}, res) {
+        console.log(body);
+        Comment.create(body)
+            .then(({ _id }) => {
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $push: { comments: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if(!dbPizzaData) {
+                    res.status(404).json({ message: 'Pizza not found' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // remove comment
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+            .then(deletedComment => {
+                if(!deletedComment) {
+                    return res.status(404).json({ message: 'Comment not found' });
+                }
+
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $pull: { comments: params.commentId } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if(!dbPizzaData) {
+                    res.status(404).json({ message: 'Pizza not found' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    }
+}
+
+module.exports = commentController;
